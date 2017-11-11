e9dda256e1a75bbed5b918b107af365bd2cc6a06
Victor Villacis <victor.villacis@outlook.com>
2017-11-11 16:13:44 -0500

    compiled succesfully, created static search and business components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
@@ -7,11 +6,13 @@ import SearchBar from './components/SearchBar/SearchBar';
 class App extends Component {
   render() {
     return (
+    
       <div className="App">
-        <h1>ravenous</h1>
-      {<BusinessList/>}
+        <h1>Ravenous</h1>
       {<SearchBar/>}
+      {<BusinessList />}
       </div>
+      
     )
 
   }
